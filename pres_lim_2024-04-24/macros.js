0a62471fcc96c83e228f54128f08453a0ce4648a
Kevin Cazelles <kevin.cazelles@insileco.io>
2024-04-02 18:51:58 -0400

    pres layout

---
+++ pres_lim_2024-04-24/macros.js
@@ -0,0 +1,31 @@
+// Add logo
+remark.macros.addlogo = function (percentage) {
+  var url = "img/logo.png";
+  return '<img src="' + url + '" style="width: ' + percentage + '" />';
+};
+
+
+// Scale image (available on Remarkjs GH page)
+remark.macros.scale = function (percentage) {
+  var url = this;
+  return '<img src="' + url + '" style="width: ' + percentage + '" />';
+};
+
+// Scale image (available on Remarkjs GH page)
+remark.macros.scalec = function (percentage) {
+  var url = this;
+  return '<span class = "center"><img src="' + url + '" style="width: ' + percentage + '" /></span>';
+};
+
+
+// toupper (see https://github.com/gnab/remark/issues/72)
+remark.macros.upper = function () {
+  // `this` is the value in the parenthesis, or undefined if left out
+  return this.toUpperCase();
+};
+
+
+remark.macros.custom_hr = function(width, height) {
+  return '<html><div style="float:left"></div><hr color="#ffdd55" style="margin-top:-60px" size=1px width=720px></html>'
+}
+
